dac0b9f497eadbd2d4dab7d084bcfe6954dbd493
ashnewport <chickensoup64@gmail.com>
2019-10-01 21:42:04 +1000

    Add or remove up to 3 experiences

--- src/components/resume-form/form/Experience.vue
+++ src/components/resume-form/form/Experience.vue
@@ -1,11 +1,22 @@
 <template>
     <div>
         <h1>Add Experience</h1>
-        <label>Company Name <input v-model="companyName" placeholder="Company Name"></label>
-        <label>Job Title <input v-model="roleName" placeholder="Your Role"></label>
-        <label>Job Information <input v-model="roleExperience" placeholder="Explain Your Role"></label>
-        <label>Start Date <input v-model="startDate" placeholder="Start Date"></label>
-        <label>End Date <input v-model="endDate" placeholder="End Date"></label>
+        <div v-show="experiences.length < 3">
+            <label>Company Name <input v-model="companyName" placeholder="Company Name"></label>
+            <label>Job Title <input v-model="roleName" placeholder="Your Role"></label>
+            <label>Job Information <input v-model="roleExperience" placeholder="Explain Your Role"></label>
+            <label>Start Date <input v-model="startDate" placeholder="Start Date"></label>
+            <label>End Date <input v-model="endDate" placeholder="End Date"></label>
+            <button @click="addExperience">Add another experience</button>
+        </div>
+        <ul>
+            <li v-for="(experience, index) in experiences" :key="index">
+                {{ experience.startDate }} - {{ experience.endDate }}<br/>
+                {{ experience.companyName }}, {{ experience.roleName }}<br/>
+                {{ experience.roleExperience }}<br/>
+                <button @click="removeExperience(index)">Remove</button>
+            </li> 
+        </ul>
     </div>
 </template>
 <script>
@@ -17,8 +28,28 @@ export default {
             roleName: "",
             roleExperience: "",
             startDate: "",
-            endDate: ""
+            endDate: "",
+            experiences: []
         };
+    },
+    methods: {
+        addExperience() {
+            this.experiences.push({
+                companyName: this.companyName,
+                roleName: this.roleName,
+                roleExperience: this.roleExperience,
+                startDate: this.startDate,
+                endDate: this.endDate
+            });
+            this.companyName = "";
+            this.roleName = "";
+            this.roleExperience = "";
+            this.startDate = "";
+            this.endDate = "";
+        },
+        removeExperience(index) {
+            this.experiences.splice(index, 1);
+        }
     }
 }
 </script>
